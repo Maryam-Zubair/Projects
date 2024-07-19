9c0f43d84255036e20132f54e065e3a4a3437a19
Maryam <123619589+Maryam-Zubair@users.noreply.github.com>
2024-07-19 14:15:31 -0700

    Update ImageGallery.js

--- Photo Gallery/photo-frontend/src/components/ImageGallery.js
+++ Photo Gallery/photo-frontend/src/components/ImageGallery.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 
-const API_BASE_URL = 'http://3.84.1.254:8000';
 
 const ImageGallery = () => {
   const location = useLocation();
@@ -16,7 +15,7 @@ const ImageGallery = () => {
       <ul>
         {photos.map((photo) => (
           <li key={photo.id}>
-            <img src={`${API_BASE_URL}${photo.image}`} alt="Family" />
+            <img src={`http://localhost:8000${photo.image}`} alt="Family" />
             <p>Added by: {photo.added_by}</p>
             <p>People in Photo: {photo.people_in_photo}</p>
             <p>Date: {new Date(photo.date_added).toLocaleDateString()}</p>
